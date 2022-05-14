102341fe443d0a518030c00db8fb8813a5d7c829
Matthew Garlington <61671107+MatthewGarlington@users.noreply.github.com>
2022-05-13 21:55:19 -0400

    Added HomeView

--- Sources/GenisisHomeFramework/GenisisHomeFramework.swift
+++
@@ -1,6 +0,0 @@
-public struct GenisisHomeFramework {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
---
+++ Sources/GenisisHomeFramework/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Matthew Garlington on 5/13/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
